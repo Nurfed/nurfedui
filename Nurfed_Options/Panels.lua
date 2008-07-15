133c363f146f4e0f4c0c22f68614c1283547f159
tivoli.nurfed <tivoli.nurfed@413f5255-fa2f-0410-a18b-3bdb7fec3447>
2008-07-15 22:31:38 +0000

    Testing again.

--- Nurfed_Options/Panels.lua
+++ Nurfed_Options/Panels.lua
@@ -4,6 +4,7 @@
 -- Test3 $Date$
 -- Test4 $date$
 -- Test5 $Rev$
+
 -- Option Menu Panels
 local hptype = { "class", "fade", "script", "pitbull" }
 local mptype = { "normal", "class", "fade", "pitbull" }
