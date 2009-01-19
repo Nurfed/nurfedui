6137bd28bb272c8b7a28a9f0de84aa453b8b14ef
Apoco@assimx.net <Apoco@assimx.net@413f5255-fa2f-0410-a18b-3bdb7fec3447>
2009-01-19 19:35:54 +0000

    Nurfed_CombatLog:  Added Options

--- wrath/Nurfed_CombatLog/Nurfed_CombatLog.lua
+++ wrath/Nurfed_CombatLog/Nurfed_CombatLog.lua
@@ -196,5 +196,258 @@ Nurfed:regevent("NURFED_LOCK", function()
 		Nurfed_SpellAlertGrid:Show()
 	end
 end)
+
+-- Config Shit!
+do
+	local function saveOpt(arg, val, noupdate)
+		local type = arg:find("spell") and "spell" or arg:find("buff") and "buff"
+		if type then
+			arg = arg:gsub(type, "")
+			NURFED_COMBATLOG_SAVED[type][arg] = val
+			if not noupdate then
+				Nurfed:sendevent("NURFED_COMBATLOG_SETTINGS_CHANGED")
+			end
+		end
+	end
+
+	local function getOpt(arg)
+		local type = arg:find("spell") and "spell" or arg:find("buff") and "buff"
+		if type then
+			arg = arg:gsub(type, "")
+			return NURFED_COMBATLOG_SAVED[type][arg]
+		end
+	end
+
+	Nurfed_CustomConfig = Nurfed_CustomConfig or {} 
+	Nurfed_CustomConfig["Nurfed_CombatLog"] = { 
+	    { 
+	        name = "Combat Log",
+	        subtext = "Combat Log Settings", 
+	        menu = {
+				-- checks
+				check1 = {
+					template = "nrf_check",
+					Anchor = { "TOPLEFT", "$parentSubText", "BOTTOMLEFT", 0, 15 },
+					vars = { text = "Spells", opt = "spellenabled", ofunc = saveOpt, getopt = getOpt, },
+				},
+				-- white castles
+				slider1 = {
+					template = "nrf_slider",
+					Anchor = { "TOPLEFT", "$parentcheck1", "BOTTOMLEFT", 0, -15 },
+					vars = {
+						text = "Width",
+						opt = "spellwidth",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 1,
+						max = 2500,
+						step = 0.5,
+						bigStep = 1.0,
+						format = "%.1f",
+					},
+				},
+				slider2 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider1value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Height",
+						opt = "spellheight",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 1,
+						max = 1000,
+						step = 0.5,
+						bigStep = 1.0,
+						format = "%.1f",
+					},
+				},
+				slider3 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider2value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Visible Time",
+						opt = "spellvisibleTime",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 0,
+						max = 10.0,
+						step = 0.05,
+						bigStep = 0.10,
+						format = "%.2f",
+					},
+				},
+				slider4 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider3value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Fade Duration",
+						opt = "spellfadeDuration",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 0,
+						max = 10,
+						step = 0.5,
+						bigStep = 1,
+						format = "%.2f",
+					},
+				},
+				slider5 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider4value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Font Height",
+						opt = "spellfontHeight",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 0.5,
+						max = 36,
+						step = 0.5,
+						bigStep = 1.5,
+						format = "%.2f",
+					},
+				},
+				-- boutons
+				button1 = {
+					template = "nrf_optbutton",
+					Anchor = { "TOP", "$parentslider5value", "BOTTOM", 0, -15 },
+					OnClick = function(self) Nurfed_DropMenu(self, {"TOP", "BOTTOM"}) end,
+					vars = { text = "Insert Mode", hint = "Change where the text is inserted.", opt = "spellinsertMode", ofunc = saveOpt, getopt = getOpt, nohitrect = true, textpos = { "BOTTOM", "$parent", "TOP", 0, 0 }, },
+				},
+				button2 = {
+					template = "nrf_optbutton",
+					Anchor = { "TOPRIGHT", "$parentbutton1", "BOTTOM", 0, -15 },
+					OnClick = function(self) Nurfed_DropMenu(self, {"OUTLINE", "NONE", "THICKOUTLINE"}) end,
+					vars = { text = "Font Style", hint = "Change the font style of the text", opt = "spellfontStyle", ofunc = saveOpt, getopt = getOpt, nohitrect = true, textpos = { "BOTTOM", "$parent", "TOP", 0, 0 }, },
+				},
+				button3 = {
+					template = "nrf_optbutton",
+					Anchor = { "TOPLEFT", "$parentbutton1", "BOTTOM", 0, -15 },
+					OnClick = function(self) Nurfed_DropMenu(self, {"LEFT", "CENTER", "RIGHT"}) end,
+					vars = { text = "Text Pos", hint = "Toggle the placement of the text", opt = "spellfontJustifyH", ofunc = saveOpt, getopt = getOpt, nohitrect = true, textpos = { "BOTTOM", "$parent", "TOP", 0, 0 }, },
+				},
+				-- buffs!
+				-- checks
+				check2 = {
+					template = "nrf_check",
+					Anchor = { "TOPRIGHT", "$parentSubText", "BOTTOMRIGHT", 0, 15 },
+					vars = { text = "Buffs", opt = "buffenabled", ofunc = saveOpt, getopt = getOpt, },
+				},
+				-- white castles
+				slider10 = {
+					template = "nrf_slider",
+					Anchor = { "TOPRIGHT", "$parentcheck2", "BOTTOMRIGHT", 0, -15 },
+					vars = {
+						text = "Width",
+						opt = "buffwidth",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 1,
+						max = 2500,
+						step = 0.5,
+						bigStep = 1.0,
+						format = "%.1f",
+					},
+				},
+				slider20 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider10value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Height",
+						opt = "buffheight",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 1,
+						max = 1000,
+						step = 0.5,
+						bigStep = 1.0,
+						format = "%.1f",
+					},
+				},
+				slider30 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider20value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Visible Time",
+						opt = "buffvisibleTime",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 0,
+						max = 10.0,
+						step = 0.05,
+						bigStep = 0.10,
+						format = "%.2f",
+					},
+				},
+				slider40 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider30value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Fade Duration",
+						opt = "bufffadeDuration",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 0,
+						max = 10,
+						step = 0.5,
+						bigStep = 1,
+						format = "%.2f",
+					},
+				},
+				slider50 = {
+					template = "nrf_slider",
+					Anchor = { "TOP", "$parentslider40value", "BOTTOM", 0, -15 },
+					vars = {
+						text = "Font Height",
+						opt = "bufffontHeight",
+						ofunc = saveOpt, 
+						getopt = getOpt,
+						numText = true,
+						min = 0.5,
+						max = 36,
+						step = 0.5,
+						bigStep = 1.5,
+						format = "%.2f",
+					},
+				},
+				-- boutons
+				button10 = {
+					template = "nrf_optbutton",
+					Anchor = { "TOP", "$parentslider50value", "BOTTOM", 0, -15 },
+					OnClick = function(self) Nurfed_DropMenu(self, {"TOP", "BOTTOM"}) end,
+					vars = { text = "Insert Mode", hint = "Change where the text is inserted.", opt = "buffinsertMode", ofunc = saveOpt, getopt = getOpt, nohitrect = true, textpos = { "BOTTOM", "$parent", "TOP", 0, 0 }, },
+				},
+				button20 = {
+					template = "nrf_optbutton",
+					Anchor = { "TOPRIGHT", "$parentbutton10", "BOTTOM", 0, -15 },
+					OnClick = function(self) Nurfed_DropMenu(self, {"OUTLINE", "NONE", "THICKOUTLINE"}) end,
+					vars = { text = "Font Style", hint = "Change the font style of the text", opt = "bufffontStyle", ofunc = saveOpt, getopt = getOpt, nohitrect = true, textpos = { "BOTTOM", "$parent", "TOP", 0, 0 }, },
+				},
+				button30 = {
+					template = "nrf_optbutton",
+					Anchor = { "TOPLEFT", "$parentbutton10", "BOTTOM", 0, -15 },
+					OnClick = function(self) Nurfed_DropMenu(self, {"LEFT", "CENTER", "RIGHT"}) end,
+					vars = { text = "Text Pos", hint = "Toggle the placement of the text", opt = "bufffontJustifyH", ofunc = saveOpt, getopt = getOpt, nohitrect = true, textpos = { "BOTTOM", "$parent", "TOP", 0, 0 }, },
+				},
+	        }, 
+	    }, 
+	} 
+	local loaded
+	Nurfed:regevent("ADDON_LOADED", function(addon) 
+		if IsAddOnLoaded("Nurfed_Options") and not loaded then 
+			Nurfed_CreateCustomConfig("Nurfed_CombatLog") 
+			loaded = true 
+		end 
+	end)
+end
+
 Nurfed:sendevent("NURFED_COMBATLOG_SETTINGS_CHANGED")
 Nurfed:setversion("Combat Log", "$Date$", "$Rev$")
